f3751f569dd51432d1a1228c14bcf9aa388d8126
HayMcK <hmckenna@chapman.edu>
2024-02-21 15:25:25 -0800

    Added

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include "Translator.h"
 
 int main(int argc, char **argv){
-    //ask if this is valid error handling technique
     if(argc == 3){
         FileProcessor f;
         f.processFile(argv[1], argv[2]);
