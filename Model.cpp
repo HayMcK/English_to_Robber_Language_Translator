9c6e4aaae9965966bca0517fb48d618efaad856b
HayMcK <hmckenna@chapman.edu>
2024-02-23 15:33:21 -0800

    Updated

--- Model.cpp
+++ Model.cpp
@@ -1,13 +1,17 @@
 #include "Model.h"
  
+//Default constructor
 Model::Model(){}
 
+//Destructor
 Model::~Model(){}
 
+//A function to translate a consonant while maintaining capitalization when needed.
 string Model::translateSingleConsonant(char cons){
     if(isupper(cons)){
         string tmp(1, toupper(cons));
-        return tmp + "O" + tmp;
+        string tmp2(1, tolower(cons));
+        return tmp + "o" + tmp2;
     }
     else{
         string tmp(1, tolower(cons));
@@ -15,6 +19,7 @@ string Model::translateSingleConsonant(char cons){
     }
 }
 
+//A function to translate a vowel while maintaining capitalization when needed.
 string Model::translateSingleVowel(char vowel){
     if(isupper(vowel)){
         string tmp(1, toupper(vowel));
