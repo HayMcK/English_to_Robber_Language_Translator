c643a59dc0ec22d72b41d561f55d41aa5ef7057a
HayMcK <hmckenna@chapman.edu>
2024-02-23 15:33:14 -0800

    Updated

--- Model.h
+++ Model.h
@@ -2,18 +2,17 @@
 #define MODEL_H
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+//A class that will encode the rules of the robber language
 class Model{
-    //A class named Model that will encode the rules of the Robber language.
     private:
     public:
         Model();
         ~Model();
-        string translateSingleConsonant(char cons);//takes a single consonant character as input and returns a string representing its encoding in Rövarspråket. Capitalization should be preserved.
-        string translateSingleVowel(char vowel);//takes a single vowel character as input and returns a string representing its encoding in Rövarspråket. Capitalization should be preserved.
+        string translateSingleConsonant(char cons);
+        string translateSingleVowel(char vowel);
         
 };
 #endif
